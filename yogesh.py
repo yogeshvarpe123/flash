02379224d5befbfc0ee079c14c91a4a813d9a92c
yogeshvarpe123 <yogeshvarpe96@gmail.com>
2022-01-05 20:26:54 +0530

    Create demo1.py

---
+++ yogesh.py
@@ -0,0 +1,9 @@
+import logging
+logging.basicConfig(filename='log.txt',level=logging.DEBUG)
+print('logging info')
+logging.debug('debug information ')
+logging.info('info information')
+logging.warning('warning information')
+logging.error('error infomation')
+logging.critical('critical infomation')
+logging.critical('critical infomation')
